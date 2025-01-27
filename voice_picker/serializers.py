214b71ae76c5c6d35b1925eca81baedf3e780835
kazuya yamamoto <83887766+mest-yamaru@users.noreply.github.com>
2025-01-28 00:11:29 +0900

    文字起こし処理の修正

--- voice_picker/serializers.py
+++ voice_picker/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import UploadedFile, Transcription
+from member_management.models import Organization
 
 class UploadedFileSerializer(serializers.ModelSerializer):
+    organization = serializers.PrimaryKeyRelatedField(queryset=Organization.objects.all(), required=False)
+
     class Meta:
         model = UploadedFile
         fields = '__all__'
