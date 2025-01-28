9486e7fd4a1c9ff46ff1b2ff601b2cb6e8b79179
kazuya yamamoto <83887766+mest-yamaru@users.noreply.github.com>
2025-01-28 23:15:12 +0900

    ログファイルをlogsディレクトリにまとめる様に修正

--- config/logging_handlers.py
+++ config/logging_handlers.py
@@ -3,13 +3,14 @@ from logging.handlers import TimedRotatingFileHandler
 from datetime import datetime
 
 class DailyRotatingFileHandler(TimedRotatingFileHandler):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, filename, *args, **kwargs):
+        self.base_filename = filename
         super().__init__(self.get_file_name(), *args, **kwargs)
 
     def get_file_name(self):
         # 現在の日付を取得し、ファイル名を生成
         date_str = datetime.now().strftime("%Y-%m-%d")
-        return f"django{date_str}.log"
+        return f"{self.base_filename}{date_str}.log"
 
     def doRollover(self):
         # ローテーション処理をカスタマイズ
