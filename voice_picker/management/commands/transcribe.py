b5ed6dfbe84b952bd7d712a4a5ba99c37c742a8c
kazuya yamamoto <83887766+mest-yamaru@users.noreply.github.com>
2025-04-20 00:02:18 +0900

    ロガー修正

--- voice_picker/management/commands/transcribe.py
+++ voice_picker/management/commands/transcribe.py
@@ -18,19 +18,19 @@ class Command(BaseCommand):
         )
 
         if not unprocessed_files.exists():
-            logger.info('文字起こしするファイルがありませんでした。')
+            processing_logger.info('文字起こしするファイルがありませんでした。')
             return
 
         file_ids = list(unprocessed_files.values_list('id', flat=True))
         UploadedFile.objects.filter(id__in=file_ids).update(status=Status.IN_PROGRESS)
-        logger.info(f'{len(file_ids)}件のファイルを処理中に設定しました。')
+        processing_logger.info(f'{len(file_ids)}件のファイルを処理中に設定しました。')
 
         for file_id in file_ids:
             try:
                 try:
                     uploaded_file = UploadedFile.objects.get(id=file_id)
                 except UploadedFile.DoesNotExist:
-                    logger.error(f"ファイルが見つかりませんでした。File ID: {file_id}")
+                    processing_logger.error(f"ファイルが見つかりませんでした。File ID: {file_id}")
                     continue
 
                 file_path = uploaded_file.file.path
@@ -39,7 +39,7 @@ class Command(BaseCommand):
                     transcribe_result = transcribe_and_save(file_path, file_id)
                     if not transcribe_result:
                         UploadedFile.objects.filter(id=file_id).update(status=Status.UNPROCESSED)
-                        logger.error(f"文字起こしに失敗しました。File ID: {file_id}")
+                        processing_logger.error(f"文字起こしに失敗しました。File ID: {file_id}")
                         continue
 
                     result = text_generation_save(uploaded_file)
@@ -48,11 +48,11 @@ class Command(BaseCommand):
 
                     UploadedFile.objects.filter(id=file_id).update(status=Status.PROCESSED)
 
-                logger.info(f'正常に文字起こしが完了しました。File ID: {file_id}')
+                processing_logger.info(f'正常に文字起こしが完了しました。File ID: {file_id}')
 
             except Exception as e:
                 UploadedFile.objects.filter(id=file_id).update(status=Status.UNPROCESSED)
-                logger.exception(f"文字起こしでエラーが発生しました。File ID: {file_id}")
-                logger.error(f'文字起こしでエラーが発生しました: {e}')
+                processing_logger.exception(f"文字起こしでエラーが発生しました。File ID: {file_id}")
+                processing_logger.error(f'文字起こしでエラーが発生しました: {e}')
 
-        logger.info('全ファイルの文字起こし処理が完了しました。')
+        processing_logger.info('全ファイルの文字起こし処理が完了しました。')
