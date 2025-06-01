e4655119a26d182daa3be8e7dc48e70b6d70f609
kazuya yamamoto <83887766+mest-yamaru@users.noreply.github.com>
2025-06-01 23:47:19 +0900

    settings.py戻し、文字起こし保存API実装

--- voice_picker/api_urls.py
+++ voice_picker/api_urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework import routers
-from .views import UploadedFileViewSet
+from .views import UploadedFileViewSet, TranscriptionViewSet
 from django.views.decorators.csrf import csrf_exempt
 
 router = routers.DefaultRouter()
@@ -8,6 +8,7 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('upload-files/total-duration/', csrf_exempt(UploadedFileViewSet.as_view({'post': 'total_duration'})), name='total_duration'),
     path('upload-files/audio/<uuid:pk>/', csrf_exempt(UploadedFileViewSet.as_view({'get': 'audio'})), name='audio'),
+    path('transcriptions/save-transcriptions/', csrf_exempt(TranscriptionViewSet.as_view({'post': 'save_transcriptions'})), name='save_transcriptions'),
 ]
 
 urlpatterns += router.urls
